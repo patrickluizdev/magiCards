0722d245cab51673768173244dd0f45d1b50f569
Jatabara <patrickluizjf@hotmail.com>
2022-07-13 10:10:15 -0300

    Organização e melhorias no codigo

--- src/script.js
+++ src/script.js
@@ -23,7 +23,7 @@ function init() {
 
 function questOne() {
     spanOne.innerHTML = '<h2> Verifique se esté card contem o seu Numero  ↓</h2> ';
-    spanImg.innerHTML = '<img src="src/img/01.jpeg" alt="cartao" id="cartao">';
+    spanImg.innerHTML = '<img src="src/img/card00.jpeg" alt="cartao" id="cartao">';
     button()
     document.getElementById('toGo').onclick = function() {
         calcOne();
@@ -38,7 +38,7 @@ function questOne() {
 function questTwo() {
     document.getElementById("cartao").src = "https://gifs.eco.br/wp-content/uploads/2021/08/imagens-e-gifs-de-loading-19.gif";
 
-    setTimeout(`document.getElementById("cartao").src = "src/img/02.jpeg"`, 2000);
+    setTimeout(`document.getElementById("cartao").src = "src/img/card01.jpeg"`, 2000);
 
     document.getElementById('toGo').onclick = function() {
         questThree();
@@ -54,7 +54,7 @@ function questTwo() {
 function questThree() {
     document.getElementById("cartao").src = "https://gifs.eco.br/wp-content/uploads/2021/08/imagens-e-gifs-de-loading-19.gif"
 
-    setTimeout(`document.getElementById("cartao").src = "src/img/04.jpeg"`, 2000);
+    setTimeout(`document.getElementById("cartao").src = "src/img/card02.jpeg"`, 2000);
 
     document.getElementById('toGo').onclick = function() {
         questFour();
@@ -68,7 +68,7 @@ function questThree() {
 function questFour() {
     document.getElementById("cartao").src = "https://gifs.eco.br/wp-content/uploads/2021/08/imagens-e-gifs-de-loading-19.gif"
 
-    setTimeout(`document.getElementById("cartao").src = "src/img/08.jpeg"`, 2000);
+    setTimeout(`document.getElementById("cartao").src = "src/img/card03.jpeg"`, 2000);
 
     document.getElementById('toGo').onclick = function() {
         questFive();
@@ -83,7 +83,7 @@ function questFour() {
 function questFive() {
     document.getElementById("cartao").src = "https://gifs.eco.br/wp-content/uploads/2021/08/imagens-e-gifs-de-loading-19.gif"
 
-    setTimeout(`document.getElementById("cartao").src = "src/img/16.jpeg"`, 2000);
+    setTimeout(`document.getElementById("cartao").src = "src/img/card04.jpeg"`, 2000);
 
     document.getElementById('toGo').onclick = function() {
         questSix();
@@ -97,7 +97,7 @@ function questFive() {
 function questSix() {
     document.getElementById("cartao").src = "https://gifs.eco.br/wp-content/uploads/2021/08/imagens-e-gifs-de-loading-19.gif"
 
-    setTimeout(`document.getElementById("cartao").src = "src/img/32.jpeg"`, 2000);
+    setTimeout(`document.getElementById("cartao").src = "src/img/card05.jpeg"`, 2000);
 
     document.getElementById('toGo').onclick = function() {
         questSeven();
@@ -106,11 +106,12 @@ function questSix() {
     document.getElementById('isNot').onclick = function() {
         questSeven();
     }
+    number - ((5 ^ -4 * 9 - 3 - 9 * -4 - 8) / 2) > 100 ? result() : teste();
 }
 
 function questSeven() {
     document.getElementById("cartao").src = "https://gifs.eco.br/wp-content/uploads/2021/08/imagens-e-gifs-de-loading-19.gif"
-    setTimeout(`document.getElementById("cartao").src = "src/img/64.jpeg"`, 2000);
+    setTimeout(`document.getElementById("cartao").src = "src/img/card06.jpeg"`, 2000);
 
     document.getElementById('toGo').onclick = function() {
         calcSeven();
@@ -126,6 +127,8 @@ function questSeven() {
 function result() {
     spanOne.innerHTML = '  '
     btn.innerHTML = '  '
+    btNot.innerHTML = '  '
+    btnGo.innerHTML = '  '
     spanImg.innerHTML = '  '
     spanOne.innerHTML = ' <span> Seu numero é o: <br> ↓ <br> ' + number + '</span> ';
 
@@ -134,33 +137,45 @@ function result() {
 ///////////////////////////////////////////////////////////////////
 
 function calcOne() {
-    number = number + (5 ^ 2 + 43 - 3 ^ 3 + 6 ^ 2 - 6 ^ 2 - 4 * 9 - 3)
+
+    number = number + (5 ^ -4 * 9 - 3 - 9 * -4 - 8 * 8 + 16 + 106 + 14);
 }
 
 function calcTwo() {
-    number = number + (5 ^ 2 - 3 ^ 3 + 6 ^ 2 - 4 * 9 - 3 - 9 + 30 - 1 + 2)
+
+    number = number + ((5 ^ -4 * 9 - 3 - 9 * -4 - 8 * 8 + 16 + 106 + 14) / 2);
 }
 
 function calcThree() {
-    number = number + (5 ^ 2 - 3 ^ 3 + 6 ^ 2 - 4 * 9 - 3 - 9 * -4 - 8)
+
+    number = number + (5 ^ 2 - 3 ^ 3 + 6 ^ 2 - 4 * 9 - 3 - 9 * -4 - 8);
+    number + (5 ^ 2 - 3 ^ 3 + 6 ^ 2 - 4 * 9 - 3 - 9 + 30 - 1 + 2) >= 100 ? result() : teste();
+
 }
 
 function calcFour() {
-    number = number - (5 ^ -4 * 9 - 3 - 9 * -4 - 8) / 2
+
+    number = number + (5 ^ 2 - 3 ^ 3 + 6 ^ 2 - 4 * 9 - 3 - 9 + 30 - 1 + 2);
+
 }
 
 
 
 function calcFive() {
-    number = number - (5 ^ -4 * 9 - 3 - 9 * -4 - 8)
+
+    number = number + (5 ^ 2 + 43 - 3 ^ 3 + 6 ^ 2 - 6 ^ 2 - 4 * 9 - 3);
+    number - (5 ^ -4 * 9 - 3 - 9 * -4 - 8) > 100 ? result() : teste();
 }
 
 function calcSix() {
-    number = number + (5 ^ -4 * 9 - 3 - 9 * -4 - 8 * 8 + 16 + 106 + 14) / 2
+
+    number = number - (5 ^ -4 * 9 - 3 - 9 * -4 - 8);
+
 }
 
 function calcSeven() {
-    number = number + (5 ^ -4 * 9 - 3 - 9 * -4 - 8 * 8 + 16 + 106 + 14)
+
+    number = number - ((5 ^ -4 * 9 - 3 - 9 * -4 - 8) / 2);
 }
 
 
@@ -172,3 +187,19 @@ function teste() {
 function reload() {
     location.reload();
 }
+
+//number = number + (5 ^ 2 + 43 - 3 ^ 3 + 6 ^ 2 - 6 ^ 2 - 4 * 9 - 3)
+
+//number = number + (5 ^ 2 - 3 ^ 3 + 6 ^ 2 - 4 * 9 - 3 - 9 + 30 - 1 + 2)
+
+
+// number = number + (5 ^ 2 - 3 ^ 3 + 6 ^ 2 - 4 * 9 - 3 - 9 * -4 - 8)
+
+// number = number - (5 ^ -4 * 9 - 3 - 9 * -4 - 8) / 2
+
+//   number = number - (5 ^ -4 * 9 - 3 - 9 * -4 - 8)
+
+//  number = number + (5 ^ -4 * 9 - 3 - 9 * -4 - 8 * 8 + 16 + 106 + 14) / 2
+
+
+//  number = number + (5 ^ -4 * 9 - 3 - 9 * -4 - 8 * 8 + 16 + 106 + 14)
